a55ae439b6de3c0432d2e0342afb076ab1e91b31
Abhiraj3112000 <chatterjeeabhirajofficial@gmail.com>
2021-07-27 05:40:59 +0530

    initial commit

---
+++ build/precache-manifest.8ad93e7e3a530560b11166746764bff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255544eac7dc37bfe5fc87aeb2e10552",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6928e75272d8de953d",
+    "url": "/static/css/main.bb75c0ca.chunk.css"
+  },
+  {
+    "revision": "826486fd94e9e403ba20",
+    "url": "/static/js/2.79200114.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.79200114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6928e75272d8de953d",
+    "url": "/static/js/main.852791e2.chunk.js"
+  },
+  {
+    "revision": "10d02fea98c9ca780f70",
+    "url": "/static/js/runtime-main.e63f9622.js"
+  }
+]);
